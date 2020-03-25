329f3cb30d144df9d0fc58d4a4b6d16cc6e9999c
oabanin-HOME <juristaba@gmail.com>
2020-03-26 00:03:51 +0300

    add ShoppingCartTable reducer to component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,47 @@
 const initialState = {
-    books : [],
+    books: [],
     loading: true,
-    error: null
+    error: null,
+    cartItems: [
+        {
+            id: 1,
+            name: 'Bokk1',
+            count: 3,
+            total: 150
+        },
+        {
+            id: 2,
+            name: 'Bokk33',
+            count: 1,
+            total: 133
+        }
+    ],
+    orderTotal: 100
 }
 
-const reducer = (state = initialState, action) =>  {
-    switch(action.type){
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
 
         case 'FETCH_BOOKS_REQUEST':
             return {
-                books : [],
+                ...state,
+                books: [],
                 loading: true,
                 error: null
             }
-        
+
         case 'FETCH_BOOKS_SUCCESS':
             return {
+                ...state,
                 books: action.payload,
                 loading: false,
                 error: null
             }
-        
+
         case 'FETCH_BOOKS_FAILURE':
             return {
-                books : [],
+                ...state,
+                books: [],
                 loading: false,
                 error: action.payload
             }
