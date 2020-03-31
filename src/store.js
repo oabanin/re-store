83d92131daea3647bad8c68683cfa09a4c6d1633
oabanin-WORK <juristaba@gmail.com>
2020-03-31 10:39:33 +0400

    Monkey patching

--- src/store.js
+++ src/store.js
@@ -4,4 +4,15 @@ import reducer from './reducers';
 
 const store = createStore(reducer);
 
+const originalDispatch = store.dispatch;
+
+store.dispatch = (action) => {
+    if(typeof action === "string"){
+        return originalDispatch({type: action})
+    }
+    return originalDispatch(action);
+}
+
+store.dispatch({type: 'HELLO_WORLD'});
+
 export default store;
